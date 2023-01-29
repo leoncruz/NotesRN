e78c8a5608f71dd578ecd1f24ff115bb8738884a
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-01-29 08:41:46 -0300

    install and configure reanimated and gesture-handler

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NativeBaseProvider } from 'native-base';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Home } from '../Home';
 import { Note } from '../Note';
@@ -11,18 +12,21 @@ const Stack = createNativeStackNavigator();
 
 const App: React.FC = () => {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Notes" component={Home} />
-          <Stack.Screen
-            name="Note"
-            component={Note}
-            options={({ route }) => ({ title: route.params?.name })}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    // eslint-disable-next-line react-native/no-inline-styles
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Notes" component={Home} />
+            <Stack.Screen
+              name="Note"
+              component={Note}
+              options={({ route }) => ({ title: route.params?.name })}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </GestureHandlerRootView>
   );
 };
 
