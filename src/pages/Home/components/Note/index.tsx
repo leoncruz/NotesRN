07406907e3ac28e31be970e31142e7b4e468ee07
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-01-29 08:11:21 -0300

    navigate to Note screen

--- src/pages/Home/components/Note/index.tsx
+++ src/pages/Home/components/Note/index.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { Box, Column, Text } from 'native-base';
+import { Box, Column, Pressable, Text } from 'native-base';
 
 import { Note as NoteModel } from '../../../../models/Note';
+import { useNavigation } from '@react-navigation/native';
 
 type NoteProps = {
   note: NoteModel;
 };
 
 const Note: React.FC<NoteProps> = ({ note }) => {
+  const navigation = useNavigation();
+
+  const showNote = () => {
+    navigation.navigate('Note' as never, { note, name: note.title } as never);
+  };
+
   return (
-    <Box bg="white" h={100} margin={3} borderRadius={10} padding={5} shadow={4}>
-      <Column>
-        <Text>{note.title}</Text>
-        <Text paddingTop={5} maxH={50} fontSize={16} numberOfLines={1}>
-          {note.body}
-        </Text>
-      </Column>
-    </Box>
+    <Pressable onPress={showNote}>
+      <Box bg="white" margin={3} borderRadius={10} padding={5} shadow={4}>
+        <Column>
+          <Text>{note.title}</Text>
+          <Text paddingTop={5} maxH={50} fontSize={16} numberOfLines={1}>
+            {note.body}
+          </Text>
+        </Column>
+      </Box>
+    </Pressable>
   );
 };
 
